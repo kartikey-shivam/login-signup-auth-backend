4589add3d1e6e6e40dd4d300f5d6b56a05bd621c
shivam saraswat <shivam@gmail.com>
2022-12-15 18:24:17 +0530

    first commit

--- app.js
+++ app.js
@@ -2,18 +2,14 @@ const express = require('express');
 const cookieParser = require('cookie-parser');
 
 const app = express();
-const product = require('./routes/productRoute');
 const user = require('./routes/userRoute');
-const order = require('./routes/orderRoute');
 const globalErrorHandler = require('./controller/errorController');
 
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.static(`${__dirname}/public`));
 
-app.use('/api/v1', product);
 app.use('/api/v1', user);
-app.use('/api/v1', order);
 
 app.use(globalErrorHandler);
 
